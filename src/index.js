09d8e0f4d436e5fa60e28a634035d50de71b15ec
LandLena2 <e.badmazhapova@mail.ru>
2023-06-04 23:05:11 +0300

    FLUX

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {RouterProvider} from "react-router-dom";
 import router from './router';
+import { Provider as StoreProvider } from "react-redux";
+import { store } from './srore';
 
 
  
@@ -13,7 +15,9 @@ import router from './router';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <StoreProvider store = {store}>
     <RouterProvider router={router}/>
+    </StoreProvider>
   </React.StrictMode>
 );
 
