af40e17df071e49e5f67bfa7ea939d952626ee46
LandLena2 <e.badmazhapova@mail.ru>
2023-06-08 22:19:51 +0300

    20230608

--- src/pages/MenuPages/index.js
+++ src/pages/MenuPages/index.js
@@ -1,14 +1,25 @@
 import { useOutletContext } from "react-router-dom";
 import MenuCard from "../../components/MenuCard";
-import menu from "../../fakeData";
+import { useDispatch, useSelector } from "react-redux";
+import { loadMenu } from "../../srore/slices/menuSlice";
+
 
 const Menu = () => {
 
+    const menu = useSelector(state => state.menu)
+    const dispatch = useDispatch();
+    dispatch(loadMenu());
+
+   // useEffect(()=>{
+
+    //},
+
+   // [])
 
     return (
 
         <div className="mx-auto grid gap-2 grid-cols-2 md:grid-cols-3 xl:grid-cols-4 container">
-            {menu.map((item) => (
+            {menu.items.map((item) => (
 
                 <MenuCard item={item} />
 
--- src/srore/index.js
+++ src/srore/index.js
@@ -1,10 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./slices/cartSlice";
 
+import menuSlice from "./slices/menuSlice";
+
 
 export const store = configureStore({
 
     reducer: {
-        cart: cartReducer
+        cart: cartReducer,
+        menu:menuSlice
     }
 });
